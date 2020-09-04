b1930b8fcb46beb1fac30d77f361d5aa61d8546b
Ben Burdette <bburdette@gmail.com>
2020-09-04 14:06:27 -0600

    public-ify things

--- api/rust/src/protocol.rs
+++ api/rust/src/protocol.rs
@@ -9,86 +9,76 @@ pub struct ServerResponse {
 #[derive(Deserialize, Debug)]
 pub struct UserMessage {
   pub uid: String,
-  pwd: String,
-  what: String,
-  data: Option<serde_json::Value>,
+  pub pwd: String,
+  pub what: String,
+  pub data: Option<serde_json::Value>,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct PublicMessage {
-  what: String,
-  data: Option<serde_json::Value>,
+  pub what: String,
+  pub data: Option<serde_json::Value>,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct RegistrationData {
-  email: String,
+  pub email: String,
 }
 
 #[derive(Serialize, Debug, Clone)]
 pub struct Device {
-  id: i64,
-  user: i64,
-  name: String,
-  description: String,
-  createdate: i64,
-  changeddate: i64,
+  pub id: i64,
+  pub user: i64,
+  pub name: String,
+  pub description: String,
+  pub createdate: i64,
+  pub changeddate: i64,
 }
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct SaveDevice {
-  id: Option<i64>,
-  name: String,
-  description: String,
+  pub id: Option<i64>,
+  pub name: String,
+  pub description: String,
 }
 
 #[derive(Serialize, Debug, Clone)]
 pub struct Sensor {
-  id: i64,
-  device: i64,
-  name: String,
-  description: String,
-  createdate: i64,
-  changeddate: i64,
+  pub id: i64,
+  pub device: i64,
+  pub name: String,
+  pub description: String,
+  pub createdate: i64,
+  pub changeddate: i64,
 }
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct SaveSensor {
-  id: Option<i64>,
-  device: i64,
-  name: String,
-  description: String,
+  pub id: Option<i64>,
+  pub device: i64,
+  pub name: String,
+  pub description: String,
 }
 
 #[derive(Serialize, Debug, Clone)]
 pub struct Measurement {
-  id: i64,
-  value: f64,
-  sensor: i64,
-  createdate: i64,
-  measuredate: i64,
+  pub id: i64,
+  pub value: f64,
+  pub sensor: i64,
+  pub createdate: i64,
+  pub measuredate: i64,
 }
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct SaveMeasurement {
-  value: f64,
-  sensor: i64,
-  measuredate: i64,
+  pub value: f64,
+  pub sensor: i64,
+  pub measuredate: i64,
 }
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct MeasurementQuery {
   pub sensor: i64,
-  enddate: Option<i64>,
-  lengthOfTime: Option<i64>,
+  pub enddate: Option<i64>,
+  pub lengthOfTime: Option<i64>,
 }
-
-// #[derive(Deserialize, Serialize, Debug)]
-// pub struct User {
-//   pub id: i64,
-//   pub name: String,
-//   pub hashwd: String,
-//   pub salt: String,
-//   pub email: String,
-//   pub registration_key: Option<String>,
-// }
